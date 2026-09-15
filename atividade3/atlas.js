fded7ff86492d11c29e8dfdb8b3bed3d3fbbfba8
thiagoeliassantos18-debug <thiago.elias.santos18@escola.pr.gov.br>
2026-09-15 08:51:06 -0300

    Add files via upload

---
+++ atividade3/atlas.js
@@ -0,0 +1,13 @@
+function calcular(){
+   let nota1trim = Number(prompt("Digiti a nota do primeiro trimestre"))
+   let nota2trim = Number(prompt("Digiti a nota dosegundo trimestre"))
+
+   let resultado = 180-(nota1trim+nota2trim)
+   alert("Voce precisa de" + resultado + "para ser aprovado");
+
+   if(resultado >= 180){
+      alert("parabens voce foi aprovado por nota!!");
+     } else {
+      alert("voce precisa de" );
+     }
+}
