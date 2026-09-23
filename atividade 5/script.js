573bbc8abbc2cf393633b1d8c7b392f7ebd61475
thiagoeliassantos18-debug <thiago.elias.santos18@escola.pr.gov.br>
2026-09-23 12:06:11 -0300

    Add files via upload

---
+++ atividade 5/script.js
@@ -0,0 +1,16 @@
+function maior(){
+    
+
+
+
+
+
+    let numero1;
+    let numero2;
+    let resultado;
+
+    numero1 = Number(prompt("Digite o primeiro número."));
+    numero2 =  Number(prompt("Digite o segundo numero."));
+    
+
+}
---
+++ atividade6/hinto.js
@@ -0,0 +1,7 @@
+function site (){
+
+
+
+    nome = prompt("Qual é o seu nome?");
+    SpeechRecognitionResult.in
+}
